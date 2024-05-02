d22c5cc9bbd68bd7971a2af52bc4dcdabe4c80cd
nairyosangha <nairyo@cock.li>
2024-05-02 19:46:47 +0200

    pack/unpack has to work on Lua5.1 and 5.2

--- regex.lua
+++ regex.lua
@@ -3,7 +3,17 @@ function Regex(pattern, capture_groups)
 	return setmetatable({ pattern = pattern, capture_groups = capture_groups }, { __index = regex })
 end
 
+local _unpack = _VERSION == "Lua 5.1" and unpack or table.unpack
+
 function regex:match(str)
+	local function pack(...)
+		local packed = {}
+		for i=1,select('#', ...) do
+			packed[i] = select(i, ...)
+		end
+		return packed
+	end
+
 	local function _group(packed_matches)
 		local function _get_capture_idx(idx)
 			return self.capture_groups and string.byte(self.capture_groups, idx, idx) or idx
@@ -14,12 +24,12 @@ function regex:match(str)
 		end
 		return groups
 	end
-	self.matches = _group(table.pack(str:match(self.pattern)))
+	self.matches = _group(pack(str:match(self.pattern)))
 	return #self.matches > 0
 end
 
 function regex:groups()
-	return table.unpack(self.matches)
+	return _unpack(self.matches)
 end
 
 function regex:count_capture_groups()
